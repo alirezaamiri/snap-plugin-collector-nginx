e1aceec79cc2e77dfc8a21f6c42031a75639102f
Alireza Amiri <alirezaamiri.ara@gmail.com>
2017-04-12 15:04:26 +0200

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ limitations under the License.
 package main
 
 import (
-	"github.com/Staples-Inc/snap-plugin-collector-nginx/nginx"
+	"github.com/gwdg/snap-plugin-collector-nginx/nginx"
 	"github.com/intelsdi-x/snap-plugin-lib-go/v1/plugin"
 )
 
